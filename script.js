ed2f24b729942aed1e314a05f64e7db300b471f8
Godwin0397 <godveenpaul@gmail.com>
2024-11-22 17:50:00 +0530

    JS API call

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-var request = XMLHttpRequest()
+var request = new XMLHttpRequest();
 request.open("GET", "https://restcountries.com/v3.1/all")
+request.send()
+request.onload = function(){
+    var res = JSON.parse(request.response)
+    console.log(res)
+    for (let x of res){
+        console.log('Common Name:', x.name.common, 'Capital:', x.capital && x.capital[0] ? x.capital[0] : 'Capital not available', 'Area:', x.area)
+    }
+}
