d31933613ffa4ac06e2c86e729b87cf4c7bfab74
Godwin0397 <godveenpaul@gmail.com>
2024-11-25 14:22:50 +0530

    functions added in script

--- script.js
+++ script.js
@@ -8,3 +8,35 @@ request.onload = function(){
         console.log('Common Name:', x.name.common, 'Capital:', x.capital && x.capital[0] ? x.capital[0] : 'Capital not available', 'Area:', x.area)
     }
 }
+
+function getFirstData(inputData, n){
+    if (n){
+        if (n<0){
+            return []
+        }
+        else if (n>0){
+            return inputData.slice(0, n)
+        }
+    }
+    else{
+        return inputData[0]
+    }
+}
+console.log(getFirstData([4,5,8,9]))
+
+let getFirstDataAnonymousFunction = function(inputData, n){
+    if (n){
+        if (n<0){
+            return []
+        }
+        else if (n>0){
+            return inputData.slice(0, n)
+        }
+    }
+    else{
+        return inputData[0]
+    }
+}
+console.log(getFirstDataAnonymousFunction([5,8,9,6,1,4],5))
+
+
